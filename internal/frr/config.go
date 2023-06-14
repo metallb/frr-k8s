0ad8895e96f2e3bce7eca6ca482cf402b450d1f5
Federico Paolinelli <fpaoline@redhat.com>
2023-06-14 22:26:46 +0200

    Add the frr module
    The FRR module is implemented the same way (copied) as MetalLB. The only
    difference is the starting point, which is the frr-k8s api.
    
    We have a translation layer that takes the FRR-K8s config, then
    translates it, and then generate the FRR configuration and calls the
    reloader script.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ internal/frr/config.go
@@ -0,0 +1,263 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package frr
+
+import (
+	"bytes"
+	"context"
+	"embed"
+	"fmt"
+	"os"
+	"reflect"
+	"strconv"
+	"syscall"
+	"text/template"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/metallb/frrk8s/internal/ipfamily"
+	"github.com/pkg/errors"
+)
+
+var (
+	configFileName      = "/etc/frr_reloader/frr.conf"
+	reloaderPidFileName = "/etc/frr_reloader/reloader.pid"
+	//go:embed templates/* templates/*
+	templates embed.FS
+)
+
+type Config struct {
+	Loglevel    string
+	Hostname    string
+	Routers     []*RouterConfig
+	BFDProfiles []BFDProfile
+	ExtraConfig string
+}
+
+type reloadEvent struct {
+	config *Config
+	useOld bool
+}
+
+type RouterConfig struct {
+	MyASN        uint32
+	RouterID     string
+	Neighbors    []*NeighborConfig
+	VRF          string
+	IPV4Prefixes []string
+	IPV6Prefixes []string
+}
+
+type BFDProfile struct {
+	Name             string
+	ReceiveInterval  *uint32
+	TransmitInterval *uint32
+	DetectMultiplier *uint32
+	EchoInterval     *uint32
+	EchoMode         bool
+	PassiveMode      bool
+	MinimumTTL       *uint32
+}
+
+type NeighborConfig struct {
+	IPFamily            ipfamily.Family
+	Name                string
+	ASN                 uint32
+	SrcAddr             string
+	Addr                string
+	Port                uint16
+	HoldTime            uint64
+	KeepaliveTime       uint64
+	Password            string
+	Advertisements      []*AdvertisementConfig
+	BFDProfile          string
+	EBGPMultiHop        bool
+	VRFName             string
+	HasV4Advertisements bool
+	HasV6Advertisements bool
+}
+
+func (n *NeighborConfig) ID() string {
+	if n.VRFName == "" {
+		return n.Addr
+	}
+	return fmt.Sprintf("%s-%s", n.Addr, n.VRFName)
+}
+
+type AdvertisementConfig struct {
+	IPFamily    ipfamily.Family
+	Prefix      string
+	Communities []string
+	LocalPref   uint32
+}
+
+// templateConfig uses the template library to template
+// 'globalConfigTemplate' using 'data'.
+func templateConfig(data interface{}) (string, error) {
+	i := 0
+	currentCounterName := ""
+	t, err := template.New("frr.tmpl").Funcs(
+		template.FuncMap{
+			"counter": func(counterName string) int {
+				if currentCounterName != counterName {
+					currentCounterName = counterName
+					i = 0
+				}
+				i++
+				return i
+			},
+			"frrIPFamily": func(ipFamily ipfamily.Family) string {
+				if ipFamily == "ipv6" {
+					return "ipv6"
+				}
+				return "ip"
+			},
+			"localPrefPrefixList": func(neighbor *NeighborConfig, localPreference uint32) string {
+				return fmt.Sprintf("%s-%d-%s-localpref-prefixes", neighbor.ID(), localPreference, neighbor.IPFamily)
+			},
+			"communityPrefixList": func(neighbor *NeighborConfig, community string) string {
+				return fmt.Sprintf("%s-%s-%s-community-prefixes", neighbor.ID(), community, neighbor.IPFamily)
+			},
+			"allowedPrefixList": func(neighbor *NeighborConfig) string {
+				return fmt.Sprintf("%s-pl-%s", neighbor.ID(), neighbor.IPFamily)
+			},
+			"mustDisableConnectedCheck": func(ipFamily ipfamily.Family, myASN, asn uint32, eBGPMultiHop bool) bool {
+				// return true only for IPv6 eBGP sessions
+				if ipFamily == "ipv6" && myASN != asn && !eBGPMultiHop {
+					return true
+				}
+				return false
+			},
+			"dict": func(values ...interface{}) (map[string]interface{}, error) {
+				if len(values)%2 != 0 {
+					return nil, errors.New("invalid dict call, expecting even number of args")
+				}
+				dict := make(map[string]interface{}, len(values)/2)
+				for i := 0; i < len(values); i += 2 {
+					key, ok := values[i].(string)
+					if !ok {
+						return nil, fmt.Errorf("dict keys must be strings, got %v %T", values[i], values[i])
+					}
+					dict[key] = values[i+1]
+				}
+				return dict, nil
+			},
+		}).ParseFS(templates, "templates/*")
+	if err != nil {
+		return "", err
+	}
+
+	var b bytes.Buffer
+	err = t.Execute(&b, data)
+	return b.String(), err
+}
+
+// writeConfigFile writes the FRR configuration file (represented as a string)
+// to 'filename'.
+func writeConfig(config string, filename string) error {
+	return os.WriteFile(filename, []byte(config), 0600)
+}
+
+// reloadConfig requests that FRR reloads the configuration file. This is
+// called after updating the configuration.
+var reloadConfig = func() error {
+	pidFile, found := os.LookupEnv("FRR_RELOADER_PID_FILE")
+	if found {
+		reloaderPidFileName = pidFile
+	}
+
+	pid, err := os.ReadFile(reloaderPidFileName)
+	if err != nil {
+		return err
+	}
+
+	pidInt, err := strconv.Atoi(string(pid))
+	if err != nil {
+		return err
+	}
+
+	// send HUP signal to FRR reloader
+	err = syscall.Kill(pidInt, syscall.SIGHUP)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// generateAndReloadConfigFile takes a 'struct Config' and, using a template,
+// generates and writes a valid FRR configuration file. If this completes
+// successfully it will also force FRR to reload that configuration file.
+func generateAndReloadConfigFile(config *Config, l log.Logger) error {
+	filename, found := os.LookupEnv("FRR_CONFIG_FILE")
+	if found {
+		configFileName = filename
+	}
+
+	configString, err := templateConfig(config)
+	if err != nil {
+		level.Error(l).Log("op", "reload", "error", err, "cause", "template", "config", config)
+		return err
+	}
+	err = writeConfig(configString, configFileName)
+	if err != nil {
+		level.Error(l).Log("op", "reload", "error", err, "cause", "writeConfig", "config", config)
+		return err
+	}
+
+	err = reloadConfig()
+	if err != nil {
+		level.Error(l).Log("op", "reload", "error", err, "cause", "reload", "config", config)
+		return err
+	}
+	return nil
+}
+
+// debouncer takes a function that processes an Config, a channel where
+// the update requests are sent, and squashes any requests coming in a given timeframe
+// as a single request.
+func debouncer(ctx context.Context, body func(config *Config) error,
+	reload <-chan reloadEvent,
+	reloadInterval time.Duration,
+	failureRetryInterval time.Duration,
+	l log.Logger) {
+	go func() {
+		var config *Config
+		var timeOut <-chan time.Time
+		timerSet := false
+		for {
+			select {
+			case newCfg, ok := <-reload:
+				if !ok { // the channel was closed
+					return
+				}
+				if newCfg.useOld && config == nil {
+					level.Debug(l).Log("op", "reload", "action", "ignore config", "reason", "nil config")
+					continue // just ignore the event
+				}
+				if !newCfg.useOld && reflect.DeepEqual(newCfg.config, config) {
+					level.Debug(l).Log("op", "reload", "action", "ignore config", "reason", "same config")
+					continue // config hasn't changed
+				}
+				if !newCfg.useOld {
+					config = newCfg.config
+				}
+				if !timerSet {
+					timeOut = time.After(reloadInterval)
+					timerSet = true
+				}
+			case <-timeOut:
+				err := body(config)
+				if err != nil {
+					timeOut = time.After(failureRetryInterval)
+					timerSet = true
+					continue
+				}
+				timerSet = false
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
---
+++ internal/frr/debounce_test.go
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package frr
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/go-kit/log"
+)
+
+const timer = 10 * time.Millisecond
+const failureTimer = 10 * time.Millisecond
+
+func TestDebounce(t *testing.T) {
+	result := make(chan *Config, 10) // buffered to accommodate spurious rewrites
+	dummyUpdate := func(config *Config) error {
+		result <- config
+		return nil
+	}
+
+	reload := make(chan reloadEvent)
+	defer close(reload)
+	debouncer(context.Background(), dummyUpdate, reload, timer, failureTimer, log.NewNopLogger())
+	reload <- reloadEvent{config: &Config{Hostname: "1"}}
+	reload <- reloadEvent{config: &Config{Hostname: "2"}}
+	reload <- reloadEvent{config: &Config{Hostname: "3"}}
+	if len(result) != 0 {
+		t.Fatal("received update before time")
+	}
+	time.Sleep(3 * timer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated := <-result
+	if updated.Hostname != "3" {
+		t.Fatal("Config was not updated")
+	}
+
+	reload <- reloadEvent{config: &Config{Hostname: "3"}}
+	reload <- reloadEvent{config: &Config{Hostname: "4"}}
+	reload <- reloadEvent{config: &Config{Hostname: "5"}}
+	time.Sleep(3 * timer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated = <-result
+	if updated.Hostname != "5" {
+		t.Fatal("Config was not updated")
+	}
+}
+
+func TestDebounceRetry(t *testing.T) {
+	result := make(chan *Config, 10) // buffered to accommodate spurious rewrites
+	count := 0
+	dummyUpdate := func(config *Config) error {
+		count++
+		if count <= 3 {
+			return fmt.Errorf("error")
+		}
+		result <- config
+		return nil
+	}
+
+	reload := make(chan reloadEvent)
+	defer close(reload)
+	debouncer(context.Background(), dummyUpdate, reload, timer, failureTimer, log.NewNopLogger())
+
+	reload <- reloadEvent{config: &Config{Hostname: "1"}}
+	reload <- reloadEvent{config: &Config{Hostname: "2"}}
+	if len(result) != 0 {
+		t.Fatal("received update before time")
+	}
+	time.Sleep(10 * failureTimer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated := <-result
+	if updated.Hostname != "2" {
+		t.Fatal("Config was not updated")
+	}
+}
+
+func TestDebounceReuseOld(t *testing.T) {
+	result := make(chan *Config, 10) // buffered to accommodate spurious rewrites
+	dummyUpdate := func(config *Config) error {
+		result <- config
+		return nil
+	}
+
+	reload := make(chan reloadEvent)
+	defer close(reload)
+	debouncer(context.Background(), dummyUpdate, reload, timer, failureTimer, log.NewNopLogger())
+
+	reload <- reloadEvent{config: &Config{Hostname: "1"}}
+	if len(result) != 0 {
+		t.Fatal("received update before time")
+	}
+	time.Sleep(3 * timer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated := <-result
+	if updated.Hostname != "1" {
+		t.Fatal("Config was not updated")
+	}
+	// reload to see if the debouncer uses the old config
+	reload <- reloadEvent{useOld: true}
+	time.Sleep(3 * timer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated = <-result
+	if updated.Hostname != "1" {
+		t.Fatal("Config was not updated")
+	}
+}
+
+func TestDebounceSameConfig(t *testing.T) {
+	result := make(chan *Config, 10) // buffered to accommodate spurious rewrites
+	dummyUpdate := func(config *Config) error {
+		result <- config
+		return nil
+	}
+
+	reload := make(chan reloadEvent)
+	defer close(reload)
+	debouncer(context.Background(), dummyUpdate, reload, timer, failureTimer, log.NewNopLogger())
+	reload <- reloadEvent{config: &Config{Hostname: "1"}}
+	reload <- reloadEvent{config: &Config{Hostname: "2"}}
+	reload <- reloadEvent{config: &Config{Hostname: "3", Routers: []*RouterConfig{{MyASN: 23}}}}
+	if len(result) != 0 {
+		t.Fatal("received update before time")
+	}
+	time.Sleep(3 * timer)
+	if len(result) != 1 {
+		t.Fatal("received extra updates", len(result))
+	}
+	updated := <-result
+	if updated.Hostname != "3" {
+		t.Fatal("Config was not updated")
+	}
+
+	reload <- reloadEvent{config: &Config{Hostname: "3", Routers: []*RouterConfig{{MyASN: 23}}}}
+	reload <- reloadEvent{config: &Config{Hostname: "3", Routers: []*RouterConfig{{MyASN: 23}}}}
+
+	time.Sleep(3 * timer)
+	if len(result) != 0 {
+		updated := <-result
+		t.Fatalf("received extra updates: %d %s", len(result), updated.Hostname)
+	}
+}
