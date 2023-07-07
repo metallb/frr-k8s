1a04b208dc7e6311f2103274a2f79649012a9f7d
Federico Paolinelli <fpaoline@redhat.com>
2023-07-07 10:16:00 +0200

    E2E: add announcement tests
    We add basic announcement tests where all the nodes announce the same
    ips to all / some neighbors.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ e2etests/pkg/k8s/nodes.go
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package k8s
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clientset "k8s.io/client-go/kubernetes"
+)
+
+// Nodes returns all nodes in the cluster.
+func Nodes(cs clientset.Interface) ([]corev1.Node, error) {
+	nodes, err := cs.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to fetch frrk8s pods")
+	}
+	return nodes.Items, nil
+}
