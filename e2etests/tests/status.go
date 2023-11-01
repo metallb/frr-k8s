3362243a595ca60cfb631cc2e8b5ded9d25d26bc
Ori Braunshtein <obraunsh@redhat.com>
2023-11-01 10:35:28 +0200

    E2E: check the last healthcheck result status
    We add a test that breaks the connectivity of workers to
    the control planes, essentialy breaking their ability to reach
    the api-server and expect to see the reset status updated.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- e2etests/tests/status.go
+++ e2etests/tests/status.go
@@ -5,6 +5,7 @@ package tests
 import (
 	"context"
 	"fmt"
+	"net"
 	"strings"
 	"time"
 
@@ -21,13 +22,20 @@ import (
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/selection"
+	"k8s.io/apimachinery/pkg/types"
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/kubernetes/test/e2e/framework"
 	admissionapi "k8s.io/pod-security-admission/api"
 )
 
-const reloadSuccess = "success"
+const (
+	reloadSuccess       = "success"
+	healthResultSuccess = "success"
+	healthResultFailed  = "failed"
+)
 
 var _ = ginkgo.Describe("Exposing FRR status", func() {
 	var cs clientset.Interface
@@ -44,6 +52,8 @@ var _ = ginkgo.Describe("Exposing FRR status", func() {
 	err = frrk8sv1beta1.AddToScheme(myScheme)
 	framework.ExpectNoError(err)
 	err = v1.AddToScheme(myScheme)
+	framework.ExpectNoError(err)
+	err = corev1.AddToScheme(myScheme)
 	framework.ExpectNoError(err)
 
 	cl, err := client.New(clientconfig, client.Options{
@@ -255,6 +265,132 @@ var _ = ginkgo.Describe("Exposing FRR status", func() {
 				}, 2*time.Minute, time.Second).ShouldNot(HaveOccurred())
 			}
 		})
+
+		ginkgo.It("Works with a config that breaks worker nodes apiserver connectivity", func() {
+			// The nodes that we want to use for this test are non-control planes
+			// that do not host a webhook endpoint to minimize disruption to the cluster.
+			ginkgo.By("Getting the non control plane nodes")
+			nonCPNodes := corev1.NodeList{}
+			nonCPReq, err := labels.NewRequirement("node-role.kubernetes.io/control-plane", selection.DoesNotExist, []string{})
+			framework.ExpectNoError(err)
+			nonCPSelector := labels.NewSelector().Add(*nonCPReq)
+			err = cl.List(context.Background(), &nonCPNodes, &client.ListOptions{LabelSelector: nonCPSelector})
+			framework.ExpectNoError(err)
+
+			ginkgo.By("Getting the nodes that are hosting the webhooks")
+			webhookPods := corev1.PodList{}
+			webhookLabels := map[string]string{"app.kubernetes.io/component": "frr-k8s-webhook-server"}
+			err = cl.List(context.Background(), &webhookPods, &client.ListOptions{
+				Namespace:     k8s.FRRK8sNamespace,
+				LabelSelector: labels.SelectorFromSet(webhookLabels)})
+			framework.ExpectNoError(err)
+
+			webhookNodes := map[string]bool{}
+			for _, p := range webhookPods.Items {
+				webhookNodes[p.Spec.NodeName] = true
+			}
+
+			ginkgo.By("Setting the test nodes")
+			testNodes := []string{}
+			for _, n := range nonCPNodes.Items {
+				if webhookNodes[n.Name] {
+					continue
+				}
+				testNodes = append(testNodes, n.Name)
+			}
+			Expect(testNodes).ToNot(BeEmpty())
+
+			ginkgo.By("Verifying the last reset result for the test nodes success")
+			Eventually(func() error {
+				for _, n := range testNodes {
+					state := frrk8sv1beta1.FRRNodeState{}
+					err = cl.Get(context.Background(), types.NamespacedName{Name: n}, &state)
+					if err != nil {
+						return err
+					}
+
+					if state.Status.LastHealthcheckResult != healthResultSuccess {
+						return fmt.Errorf("last reset result %s for node %s is not success", state.Status.LastHealthcheckResult, n)
+					}
+				}
+				return nil
+			}, 2*time.Minute, 1*time.Second).ShouldNot(HaveOccurred())
+
+			ginkgo.By("Creating a config that blocks access to the apiserver on the test nodes")
+			config := frrk8sv1beta1.FRRConfiguration{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test",
+					Namespace: k8s.FRRK8sNamespace,
+				},
+				Spec: frrk8sv1beta1.FRRConfigurationSpec{
+					NodeSelector: metav1.LabelSelector{
+						MatchExpressions: []metav1.LabelSelectorRequirement{
+							{
+								Key:      "kubernetes.io/hostname",
+								Operator: metav1.LabelSelectorOpIn,
+								Values:   testNodes,
+							},
+						},
+					},
+					Raw: frrk8sv1beta1.RawConfig{
+						Priority: 5,
+						Config:   rawConfigToBlockAPIServer(cl),
+					},
+				},
+			}
+			err = updater.Update([]corev1.Secret{}, config)
+			framework.ExpectNoError(err)
+
+			ginkgo.By("Verifying the last reset result for the test nodes is failed")
+			Eventually(func() error {
+				for _, n := range testNodes {
+					state := frrk8sv1beta1.FRRNodeState{}
+					err = cl.Get(context.Background(), types.NamespacedName{Name: n}, &state)
+					if err != nil {
+						return err
+					}
+
+					if state.Status.LastHealthcheckResult != healthResultFailed {
+						return fmt.Errorf("last reset result %s for node %s is not failed", state.Status.LastHealthcheckResult, n)
+					}
+				}
+				return nil
+			}, 5*time.Minute, 5*time.Second).ShouldNot(HaveOccurred())
+
+			Consistently(func() error {
+				for _, n := range testNodes {
+					state := frrk8sv1beta1.FRRNodeState{}
+					err = cl.Get(context.Background(), types.NamespacedName{Name: n}, &state)
+					if err != nil {
+						return err
+					}
+
+					if state.Status.LastHealthcheckResult != healthResultFailed {
+						return fmt.Errorf("last reset result %s for node %s is not failed", state.Status.LastHealthcheckResult, n)
+					}
+				}
+				return nil
+			}, 30*time.Second, 2*time.Second).ShouldNot(HaveOccurred())
+
+			ginkgo.By("Deleting the bad config the result should eventually be success")
+			err = cl.Delete(context.Background(), &config)
+			framework.ExpectNoError(err)
+
+			Eventually(func() error {
+				for _, n := range testNodes {
+					state := frrk8sv1beta1.FRRNodeState{}
+					err = cl.Get(context.Background(), types.NamespacedName{Name: n}, &state)
+					if err != nil {
+						return err
+					}
+
+					if state.Status.LastHealthcheckResult != healthResultSuccess {
+						return fmt.Errorf("last reset result %s for node %s is not success", state.Status.LastHealthcheckResult, n)
+					}
+				}
+				return nil
+			}, 2*time.Minute, 1*time.Second).ShouldNot(HaveOccurred())
+		})
 	})
 
 	ginkgo.Context("Exposing the configuration conversion status", func() {
@@ -367,4 +503,34 @@ func stringMatches(toCheck string, mustContain bool, values ...string) error {
 	}
 	return nil
 
+}
+
+func rawConfigToBlockAPIServer(cl client.Client) string {
+	raw := ""
+
+	controlPlaneNodes := corev1.NodeList{}
+	err := cl.List(context.Background(), &controlPlaneNodes, &client.ListOptions{
+		LabelSelector: labels.SelectorFromSet(map[string]string{"node-role.kubernetes.io/control-plane": ""}),
+	})
+	framework.ExpectNoError(err)
+	Expect(controlPlaneNodes.Items).ToNot(BeEmpty())
+
+	for _, n := range controlPlaneNodes.Items {
+		for _, addr := range n.Status.Addresses {
+			if addr.Type != corev1.NodeInternalIP {
+				continue
+			}
+			nodeip := net.ParseIP(addr.Address)
+			Expect(nodeip).ToNot(BeNil())
+			ip := "ip"
+			length := 32
+			if nodeip.To4() == nil {
+				ip = "ipv6"
+				length = 128
+			}
+			raw += fmt.Sprintf("%s route %s/%d blackhole\n", ip, addr.Address, length)
+		}
+	}
+
+	return raw
 }
