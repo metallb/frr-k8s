516ee1905ca48bea5f628e716668f31179e4d450
Federico Paolinelli <fpaoline@redhat.com>
2025-03-11 09:32:31 +0100

    Bump golangci lint to latest
    Also, remove the deprecated exportloopref.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -2,7 +2,7 @@
 set -o errexit
 set -x
 
-GOLANGCI_LINT_VERSION="${GOLANGCI_LINT_VERSION:-1.57.1}"
+GOLANGCI_LINT_VERSION="${GOLANGCI_LINT_VERSION:-1.64.6}"
 CMD="golangci-lint run --timeout 10m0s ./..."
 ENV="${ENV:-container}"
 
