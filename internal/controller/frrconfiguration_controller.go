7589ddd2943906646c81f4c07adf5efa55070b52
Federico Paolinelli <fpaoline@redhat.com>
2025-04-15 11:21:11 +0200

    FRRConfiguration controller: squash the events over a single key
    The logic of the controller is level driven, and as we don't really rely
    on the key of the incoming object, we can then squash all the events
    over a single key. This allows to handle scenarios of a high number of
    configurations changed all together.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- internal/controller/frrconfiguration_controller.go
+++ internal/controller/frrconfiguration_controller.go
@@ -34,6 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -97,7 +98,7 @@ func (r *FRRConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	level.Debug(r.Logger).Log("controller", "FRRConfigurationReconciler", "k8s config", dumpK8sConfigs(configs))
 
 	if len(configs.Items) == 0 {
-		err := r.applyEmptyConfig(req)
+		err := r.applyEmptyConfig()
 		if err != nil {
 			updateErrors.Inc()
 			configStale.Set(1)
@@ -135,7 +136,7 @@ func (r *FRRConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	if err != nil {
 		updateErrors.Inc()
 		configStale.Set(1)
-		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to apply the config", req.NamespacedName.String(), "error", err)
+		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to convert the config, error", err)
 		conversionResult = fmt.Sprintf("failed: %v", err)
 		return ctrl.Result{}, nil
 	}
@@ -146,7 +147,7 @@ func (r *FRRConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		updateErrors.Inc()
 		configStale.Set(1)
 		conversionResult = fmt.Sprintf("failed: %v", err)
-		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to apply the config", req.NamespacedName.String(), "error", err)
+		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to apply the config, error", err)
 		return ctrl.Result{}, err
 	}
 
@@ -156,15 +157,15 @@ func (r *FRRConfigurationReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	return ctrl.Result{}, nil
 }
 
-func (r *FRRConfigurationReconciler) applyEmptyConfig(req ctrl.Request) error {
+func (r *FRRConfigurationReconciler) applyEmptyConfig() error {
 	config, err := apiToFRR(ClusterResources{}, []net.IPNet{})
 	if err != nil {
-		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to translate the empty config", req.NamespacedName.String(), "error", err)
+		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to translate the empty config, error", err)
 		panic("failed to translate empty config")
 	}
 
 	if err := r.FRRHandler.ApplyConfig(config); err != nil {
-		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to apply the empty config", req.NamespacedName.String(), "error", err)
+		level.Error(r.Logger).Log("controller", "FRRConfigurationReconciler", "failed to apply the empty config, error", err)
 		return err
 	}
 	return nil
@@ -200,8 +201,20 @@ func (r *FRRConfigurationReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	}
 
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&frrk8sv1beta1.FRRConfiguration{}).
-		Watches(&corev1.Node{}, &handler.EnqueueRequestForObject{}).
+		Watches(&frrk8sv1beta1.FRRConfiguration{},
+			// The controller is level driven, so we squash all the frrconfiguration changes to a single key.
+			// By doing this, the controller will throttle when there are a large amount of configurations generated
+			// at the same time.
+			handler.EnqueueRequestsFromMapFunc(func(ctx context.Context, obj client.Object) []reconcile.Request {
+				return []reconcile.Request{
+					{NamespacedName: types.NamespacedName{
+						Name:      "frrconfig",
+						Namespace: obj.GetNamespace(),
+					}},
+				}
+			}),
+		).
+		For(&corev1.Node{}).
 		Watches(&corev1.Secret{}, &handler.EnqueueRequestForObject{}).
 		WithEventFilter(p).
 		Complete(r)
