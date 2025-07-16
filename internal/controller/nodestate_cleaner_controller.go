b4e2ceff8ee12663a9a712dc103d36c7607fba75
Federico Paolinelli <fpaoline@redhat.com>
2025-07-16 15:34:54 +0200

    Add an optional frrnodestate cleanup controller
    We want to delete the frrnodestate resource whenever we don't have a
    pod running, so we watch pods and we delete the resource accordingly.
    
    Here we add a new optional controller that is enabled in the webhook
    pod.
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

---
+++ internal/controller/nodestate_cleaner_controller.go
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier:Apache-2.0
+
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	frrk8sv1beta1 "github.com/metallb/frr-k8s/api/v1beta1"
+)
+
+// NodeStateCleaner reconciles Pod objects to clean up FRRNodeState resources.
+type NodeStateCleaner struct {
+	client.Client
+	Scheme    *runtime.Scheme
+	Logger    log.Logger
+	Namespace string
+}
+
+// +kubebuilder:rbac:groups=frrk8s.metallb.io,resources=frrnodestates,verbs=get;list;watch;delete
+
+func (r *NodeStateCleaner) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	level.Info(r.Logger).Log("controller", "NodeStateCleaner", "start reconcile", req.String())
+	defer level.Info(r.Logger).Log("controller", "NodeStateCleaner", "end reconcile", req.String())
+
+	pods := &corev1.PodList{}
+	if err := r.Client.List(ctx, pods, client.InNamespace(r.Namespace), client.MatchingLabels(map[string]string{
+		"app.kubernetes.io/component": "frr-k8s",
+	})); err != nil {
+		level.Error(r.Logger).Log("controller", "NodeStateCleaner", "failed to list FRR-K8s pods", err)
+		return ctrl.Result{}, err
+	}
+
+	nodesWithFRRPods := make(map[string]struct{})
+	for _, pod := range pods.Items {
+		nodesWithFRRPods[pod.Spec.NodeName] = struct{}{}
+	}
+
+	frrNodeStates := &frrk8sv1beta1.FRRNodeStateList{}
+	if err := r.Client.List(ctx, frrNodeStates); err != nil {
+		level.Error(r.Logger).Log("controller", "NodeStateCleaner", "failed to list FRRNodeStates", err)
+		return ctrl.Result{}, err
+	}
+
+	var errors []error
+	for _, nodeState := range frrNodeStates.Items {
+		if _, hasFRRPods := nodesWithFRRPods[nodeState.Name]; !hasFRRPods {
+			level.Info(r.Logger).Log("controller", "NodeStateCleaner", "deleting FRRNodeState", "name", nodeState.Name, "reason", "no FRR pods on node")
+			if err := r.Client.Delete(ctx, &nodeState); err != nil {
+				level.Error(r.Logger).Log("controller", "NodeStateCleaner", "failed to delete FRRNodeState", "name", nodeState.Name, "error", err)
+				errors = append(errors, fmt.Errorf("failed to delete FRRNodeState %s: %w", nodeState.Name, err))
+			}
+		}
+	}
+
+	if len(errors) > 0 {
+		level.Error(r.Logger).Log("controller", "NodeStateCleaner", "reconcile finished with errors", "error_count", len(errors))
+		return ctrl.Result{}, utilerrors.NewAggregate(errors)
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *NodeStateCleaner) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Pod{}).
+		Watches(&frrk8sv1beta1.FRRNodeState{}, &handler.EnqueueRequestForObject{}).
+		WithEventFilter(predicate.Funcs{
+			CreateFunc: func(e event.CreateEvent) bool {
+				_, isPod := e.Object.(*corev1.Pod)
+				return !isPod
+			},
+			UpdateFunc: func(e event.UpdateEvent) bool {
+				return false
+			},
+			GenericFunc: func(e event.GenericEvent) bool {
+				return false
+			},
+			DeleteFunc: func(e event.DeleteEvent) bool {
+				_, isPod := e.Object.(*corev1.Pod)
+				return isPod
+			},
+		}).
+		Complete(r)
+}
