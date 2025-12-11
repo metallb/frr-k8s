f9baf2dc65396a1b0568d3d28e612e0fe09b5a63
Andreas Karis <ak.karis@gmail.com>
2025-12-11 16:36:39 +0100

    Refactor: Move frr-tools to cmd directory
    Reorganize the project structure by moving frr-tools binaries to the
    standard Go cmd/ directory layout:
    - frr-tools/metrics -> cmd/metrics
    - frr-tools/status -> cmd/status
    - frr-tools/reloader -> cmd/reloader
    
    All import paths and Dockerfile references have been updated
    accordingly.
    
    Reported-at: https://github.com/metallb/frr-k8s/issues/275
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- cmd/metrics/collector/bgp.go
+++ cmd/metrics/collector/bgp.go
@@ -8,7 +8,7 @@ import (
 	"github.com/metallb/frr-k8s/internal/frr"
 	"github.com/prometheus/client_golang/prometheus"
 
-	"github.com/metallb/frr-k8s/frr-tools/metrics/vtysh"
+	"github.com/metallb/frr-k8s/cmd/metrics/vtysh"
 )
 
 var labels = []string{"peer", "vrf"}
