a72b98e8e9f059b5bdaffb6eab7e60b4bda213de
Andreas Karis <ak.karis@gmail.com>
2025-12-23 20:39:17 +0100

    lint
    Signed-off-by: Andreas Karis <ak.karis@gmail.com>

--- internal/webhooks/v1beta1/frrconfiguration_webhook.go
+++ internal/webhooks/v1beta1/frrconfiguration_webhook.go
@@ -135,22 +135,6 @@ func (v *FRRConfigValidator) Handle(ctx context.Context, req admission.Request) 
 	return admission.Allowed("").WithWarnings(warnings...)
 }
 
-type healthHandler struct{}
-
-func (h *healthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(`{"status": "ok"}`))
-	if err != nil {
-		level.Error(Logger).Log("webhook", "healthcheck", "error when writing reply", err)
-	}
-}
-
 type nodeAndConfigs struct {
 	name   string
 	labels map[string]string
